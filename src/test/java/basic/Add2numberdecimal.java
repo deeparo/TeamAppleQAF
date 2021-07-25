384061be7da4caebbf99d5b743f80bf511127fdc
deepabaliroy <deepabaliroy@deepabalis-air.lan>
2021-07-25 19:55:03 -0400

    intial message

---
+++ src/test/java/basic/Add2numberdecimal.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class Add2numberdecimal {
+
+	public static void main(String[] args) {
+		double x;
+		double y;
+		x=20.10;
+		y=8.92;
+		double sum;
+		sum=x+y;
+		System.out.println(sum);
+
+	}
+
+}
