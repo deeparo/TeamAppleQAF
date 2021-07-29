ff8889c3b7e95d666ae89de543c6d0234cfb574a
deepabaliroy <deepabaliroy@deepabalis-air.lan>
2021-07-28 21:30:40 -0400

    intial message

---
+++ src/test/java/basic/Oddnumberandevennumber.java
@@ -0,0 +1,25 @@
+package basic;
+
+public class Oddnumberandevennumber {
+
+	public static void main(String[] args) {
+		int x=100;
+		//mood---%
+	    if (x%2==0);
+		System.out.println("even number");
+	     if (x%2!=0);
+		System.out.println("odd number");
+		
+		int i;
+		//mood---%
+		for (i=20;i<40;i++) {
+			if (i%2==0);
+			System.out.println(i);
+			if (i%2!=0);
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
