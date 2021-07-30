32aa4f3fdf8628a338ebfd2bd496873f4ae81b84
deepabaliroy <deepabaliroy@deepabalis-air.lan>
2021-07-30 18:56:27 -0400

    intial messages

--- src/test/java/basic/Addition.java
+++ src/test/java/basic/Addition.java
@@ -1,13 +1,25 @@
 package basic;
 
-public class Addition {
-
-	public static void main(String[] args) {
-		int x=20;
-		int y=30;
-		int sum =x+y;
-		System.out.println(sum);
-
+public class Addition { 
+	int x;
+	int y;
+	void add() {
+	 int sum;
+	 sum=x+y;
+	 System.out.println(sum);
 	}
 
-}
+	public static void main(String[] args) {
+		 Addition a1= new Addition ();
+		 a1.x=30;
+		 a1.y=20;
+		 a1.add();
+		 
+		 
+		 
+			 
+		 }
+		
+	}
+
+
---
+++ src/test/java/basic/Fizzbuzz.java
@@ -0,0 +1,23 @@
+package basic;
+
+public class Fizzbuzz {
+
+	public static void main(String[] args) {
+		int num=10;
+		if (num%3==0&& num%5==0);
+		System.out.println("fizzbuzz");
+	     if (num%3==0);
+	     System.out.println("fizz");
+	     if (num%5==0);
+	     System.out.println("buzz");
+	     else
+	    System.out.println(num); 
+	     
+	     
+		
+		
+		
+
+	}
+
+}
