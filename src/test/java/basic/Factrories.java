2179bdfe5ced47eab97b716f46fce5596bd0c760
deepabaliroy <deepabaliroy@deepabalis-air.lan>
2021-07-30 20:25:36 -0400

    intial message

---
+++ src/test/java/basic/Factrories.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class Factrories {
+
+	public static void main(String[] args) {
+		int result=1;
+		for (int i=1;i<6;i++) {
+			result =result*i;
+			System.out.println(result);
+		}
+	
+	 
+	
+		
+	}
+	
+	}
+
+
